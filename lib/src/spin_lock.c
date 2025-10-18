0ec5b3cf1bd274713e32c3c9ab74cd78e92be784
ruslan <ruslanone57@gmail.com>
2025-10-18 20:03:13 +0700

    add:  example fix: useless def destroyed

--- lib/src/spin_lock.c
+++ lib/src/spin_lock.c
@@ -1,10 +1,5 @@
-
 #include <spink_lock.h>
 #include <stdatomic.h>
-
-#define EXPECTED_VALUE 0
-#define DESIRED_VALUE 1
-
 
 int spin_lock_init(spin_lock_t *spin_lock) {
     spin_lock->lock_flag = 0;
@@ -25,16 +20,30 @@ int spin_lock_lock(spin_lock_t *spin_lock) {
 
     while (!atomic_compare_exchange_strong(&spin_lock->lock_flag,  &expected_value, 1)) {
         expected_value = 0;
-        printf("%s\n", "locked");
-        sleep(1);
+    }
+
+    return 0;
+}
+
+int spin_lock_unlock(spin_lock_t *spin_lock) {
+    if (!spin_lock) {
+        printf("%s\n", "invalid spin_lock_t");
+        return -1;
+    }
+
+    if (!((spin_lock->lock_flag == 1) || (spin_lock->lock_flag == 0))) {
+        printf("%s\n", "spin_lock failed, bad lock_flag");
+        return -1;
+    }
+
+    int expected_value = 1;
+    if (!atomic_compare_exchange_strong(&spin_lock->lock_flag,  &expected_value, 0)) {
+        printf("%s\n", "already locked");
+        return -1;
     }
 
     return 0;
 
     
-    
-    
 }
-
-int spin_lock_unlock(spin_lock_t *spin_lock);
 
--- src/main.c
+++ src/main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <mutex.h>
 #include <spink_lock.h>
+#include <pthread.h>
+
+
+int counter = 0;
+
+spin_lock_t spin;
+
+void *thread_func(void *args) {
+	spin_lock_lock(&spin);
+	int i = 0;
+	while (i < 100000000) {
+		counter++;
+		i++;
+	}
+	spin_lock_unlock(&spin);
+}
+
+
 
 int main() {
-	spin_lock_t spin;
 	spin_lock_init(&spin);
-	
-	spin_lock_lock(&spin);
-	spin_lock_lock(&spin);
+
+	pthread_t tid1;
+	pthread_create(&tid1, NULL, thread_func, NULL);
+
+	pthread_t tid2;
+	pthread_create(&tid2, NULL, thread_func, NULL);
+
+	pthread_join(tid1, NULL);
+
+	pthread_join(tid2, NULL);
+
+
+	printf("%s, %d\n", "counter:", counter);
+
+	sleep(10000);
+
 	return 0;
 }
