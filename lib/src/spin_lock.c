a2bf199fd0d8d8f98935a033f5355c0b358bd3bf
ruslan <ruslanone57@gmail.com>
2025-10-18 19:19:29 +0700

    add: spin_lock_lock errors

--- lib/src/spin_lock.c
+++ lib/src/spin_lock.c
@@ -11,10 +11,28 @@ int spin_lock_init(spin_lock_t *spin_lock) {
 }
 
 int spin_lock_lock(spin_lock_t *spin_lock) {
+    if (!spin_lock) {
+        printf("%s\n", "invalid spin_lock_t");
+        return -1;
+    }
+
+    if (!((spin_lock->lock_flag == 1) || (spin_lock->lock_flag == 0))) {
+        printf("%s\n", "spin_lock failed, bad lock_flag");
+        return -1;
+    }
+
     int expected_value = 0;
-    int val = atomic_compare_exchange_strong(&spin_lock->lock_flag,  &expected_value, 1);
-    printf("%s, %d\n", "compared? - ", val);
+
+    while (!atomic_compare_exchange_strong(&spin_lock->lock_flag,  &expected_value, 1)) {
+        expected_value = 0;
+        printf("%s\n", "locked");
+        sleep(1);
+    }
+
     return 0;
+
+    
+    
     
 }
 
